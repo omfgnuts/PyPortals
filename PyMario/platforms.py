9a5637bc7799ee3dedffa7dfd3bf4629f6c9ae65
Nuts) <omfgnuts@gmail.com>
2011-06-13 05:58:09 +0300

    documented code a little

--- PyMario/platforms.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Nuts)'
-import pygame
-class Platform(pygame.sprite.Sprite):
-
-    def __init__(self, pos):
-        pygame.sprite.Sprite.__init__(self, self.groups)
-        self.image = pygame.image.load("data/brick_small.png")
-        self.rect = self.image.get_rect(topleft=pos)
